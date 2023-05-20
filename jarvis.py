b659930ed3917fa80638aa8f06f6339d1d083323
Napole <35283865+newjxmaster@users.noreply.github.com>
2023-05-20 02:20:35 +0000

    Update jarvis.py
    Here are the primary changes I made to the code:
    
    Import Statements: I updated the imports to use from elevenlabs import generate, play instead of from elevenlabslib import *. This is a better practice as it clearly states what functions we are using from the module, rather than importing everything.
    API Keys: The API keys are now directly assigned to variables and used in the code, rather than being passed to the ElevenLabsUser object.
    
    Voice Selection: I removed the line voice = user.get_voices_by_name("Jarvis")[0] because we're now directly providing the voice name ("Bella") when calling the generate function from Eleven Labs.
    
    Text-To-Speech Generation: I replaced voice.generate_and_play_audio(message, playInBackground=False) with the generate and play functions from Eleven Labs, as we are not using the ElevenLabsUser object.
    
    Error Handling: I've added error handling for when Google Speech Recognition fails to understand the audio. In such cases, the assistant will generate an audio message asking the user to speak more clearly or loudly.
    To get the app running, you would need the following:
    
    Python: This script is written in Python, so you will need to have it installed on your system. Python 3 is recommended.
    Libraries: The script uses the following Python libraries: speech_recognition, openai, and elevenlabs. Y
    
    API Keys: You need API keys for OpenAI and Eleven Labs. Replace 'YOUR KEY HERE' in the script with your actual keys.
    
    Hardware: You will need a microphone to capture speech.
    
    ffmpeg: The Eleven Labs library uses ffmpeg to play the generated audio. You can install it via your system's package manager. For example, on macOS, you can use Homebrew: brew install ffmpeg. 
    
    If you are using Windows, you can download it from the official website and add it to your system's PATH.
    
    Permissions: Ensure that your Python environment has the necessary permissions to access your microphone and play audio.

--- jarvis.py
+++ jarvis.py
@@ -1,52 +1,82 @@
+# Import necessary libraries
 import speech_recognition as sr
 import openai
+from elevenlabs import generate, play
 
-from elevenlabslib import *
+# Set the API keys for OpenAI and ElevenLabs
+openai.api_key = 'Your OpenAi Api Key Here'
+elevenLabsAPIKey = 'Your ElevenLabs Api Key Here'
 
-openai.api_key = 'YOUR KEY HERE'
-
-elevenLabsAPIKey = 'YOUR KEY HERE'
-
+# Instantiate the recognizer and microphone
 r = sr.Recognizer()
 mic = sr.Microphone()
-user = ElevenLabsUser(elevenLabsAPIKey)
 
-voice = user.get_voices_by_name("Jarvis")[0]
-
+# Initiate a conversation with the system's initial message
 conversation = [
-        {"role": "system", "content": "Your name is Jarvis and your purpose is to be Adam's AI assistant"},
-    ]
+    {"role": "system", "content": "Your name is Jarvis and your purpose is to be an AI assistant"},
+]
 
-
+# The while loop will run indefinitely until you manually stop the program.
 while True:
+    # Use the microphone as the source for input audio
     with mic as source:
-        r.adjust_for_ambient_noise(source) #Can set the duration with duration keyword
-        print("talk")
+        # Adjust the recognizer sensitivity to ambient noise
+        r.adjust_for_ambient_noise(source, duration=0.5)
+        # Prompt the user to speak
+        print("Talk now")
+        # Record the audio
         audio = r.listen(source)
+    try:
+        # Attempt to convert the audio into text using Google Speech Recognition
+        word = r.recognize_google(audio)
 
-    word = r.recognize_google(audio)
-
-    if "draw" in word:
-        i = word.find("draw")
-        i += 5
-        response = openai.Image.create(
-            prompt=word[i:],
-            n=1,
-            size="1024x1024"
+        # If the recognized word contains 'draw', handle it separately
+        if "draw" in word:
+            # Find the index of "draw" and get the rest of the sentence
+            i = word.find("draw")
+            i += 5
+            # Call OpenAI's image creation API to generate an image based on the text
+            response = openai.Image.create(
+                prompt=word[i:],
+                n=1,
+                size="1024x1024"
+            )
+            # Get the URL of the generated image
+            image_url = response['data'][0]['url']
+            print(word[i:])
+            print(image_url)
+        else:
+            # If the recognized word does not contain 'draw', add it to the conversation
+            conversation.append({"role": "user", "content": word})
+            # Use OpenAI's chat model to generate a response to the conversation so far
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=conversation
+            )
+            # Extract the response message from the API response
+            message = response["choices"][0]["message"]["content"]
+            # Add the AI's response to the conversation
+            conversation.append({"role": "assistant", "content": message})
+            # Use Eleven Labs to generate an audio message from the AI's response
+            audio = generate(
+                text=message[:333],
+                voice="Bella",
+                model='eleven_multilingual_v1',
+                api_key=elevenLabsAPIKey
+            )
+            # Play the generated audio
+            play(audio)
+    # If Google Speech Recognition could not understand the audio, handle the error
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+        # Generate an audio message asking the user to speak more clearly or loudly
+        audio = generate(
+            text="I'm sorry, I couldn't understand that. Could you please speak more clearly or loudly?",
+            voice="Bella",
+            model='eleven_multilingual_v1',
+            api_key=elevenLabsAPIKey
         )
-        image_url = response['data'][0]['url']
-        print(word[i:])
-        print(image_url)
-
-    else:
-        conversation.append({"role": "user", "content": word})
-
-        response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=conversation
-        )
-
-        message = response["choices"][0]["message"]["content"]
-        conversation.append({"role": "assistant", "content": message})
-        # print(message)
-        voice.generate_and_play_audio(message, playInBackground=False)
+        play(audio)
+    # If there was an error in the request to Google Speech Recognition, handle the error
+    except sr.RequestError as
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
